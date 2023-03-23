8686228b01ef9b7fb027f4b5c148252a2e0d13be
Alexey Khanevski <alexey.hanevski@triplenext.com>
2023-03-23 08:14:57 +0300

    added new param for cfg

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
         negative_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
         image = init_image_1,
         num_inference_steps = int(25),
-        strength = 0.1,
+        strength = 0.65,
         guidance_scale = 7).images
     image[0].save("sd_1.png")
 
@@ -109,6 +109,6 @@ if __name__ == "__main__":
         negative_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
         image = init_image_2,
         num_inference_steps = int(25),
-        strength = 0.1,
+        strength = 0.65,
         guidance_scale = 7).images
     image[0].save("sd_2.png")
