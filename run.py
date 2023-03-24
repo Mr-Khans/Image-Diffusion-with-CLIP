e771963eeaea35d701e8e88bdb6d47e817372f0e
Alexey Khanevski <alexey.hanevski@triplenext.com>
2023-03-24 14:02:09 +0300

    added param 2 process

--- run.py
+++ run.py
@@ -55,7 +55,6 @@ def interrogate_images(image_path_1: str, image_path_2: str) -> Tuple[List[str],
     prompt_2 = ci.interrogate_fast(image_2)
     return prompt_1, prompt_2
 
-
 def initialize_image(image_path: str) -> Image:
     """
     This function takes in the path of an image, opens it, resizes it to 512x512, and converts it to RGB format.
@@ -75,9 +74,10 @@ if __name__ == "__main__":
 
   image_path_1 = argv[1]
   image_path_2 = argv[2]
+
   #model_id = 'stabilityai/stable-diffusion-2'
-  model_id = "stabilityai/stable-diffusion-2-1"
-  #model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+  model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+
   scheduler = DPMSolverMultistepScheduler.from_pretrained(model_id, subfolder="scheduler")
 
   pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
@@ -87,14 +87,15 @@ if __name__ == "__main__":
       scheduler=scheduler
     ).to("cuda")
   pipe.enable_attention_slicing()
-  pipe.safety_checker = None
-  pipe.enable_xformers_memory_efficient_attention()
 
   prompt_1, prompt_2 = interrogate_images(image_path_1, image_path_2)
+
   init_image_1 = initialize_image(image_path_1)
   init_image_2 = initialize_image(image_path_2)
+
   prompt_1 = proof_text(prompt_1)
   prompt_2 = proof_text(prompt_2)
+
   #print(prompt_1)
   #print(prompt_2)
   seed = 121 #random.randint(0, 2147483647)
@@ -113,14 +114,20 @@ if __name__ == "__main__":
         generator = torch.Generator("cuda").manual_seed(seed)).images
     image[0].save("sd_1.png")
     print(f"prompt: {prompt_1}\nstep: {num_inference_steps}\nstrength: {strength}\nguidance_scale: {guidance_scale}\nseed: {seed}")
+
+  num_inference_steps_2 = int(35)
+  strength_2 = 0.65
+  guidance_scale_2 = 7
+
   with autocast("cuda"):
     image = pipe(
-        prompt =  prompt_1 + "by greg rutkowski, romanticism, cinematic lighting, hyperdetailed, 8 k realistic, global illumination, radiant light, trending on artstation, digital art",
+        prompt = "ultra detailed, photorealistic, " + prompt_1 + " high detail, high quality, 8K, photo realism",
         negative_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
         image = init_image_2,
-        num_inference_steps = int(50),
-        strength = 0.65,
-        guidance_scale = 9,
+        num_inference_steps = num_inference_steps_2,
+        strength = strength_2,
+        guidance_scale = guidance_scale_2,
         generator = torch.Generator("cuda").manual_seed(seed)).images
-    print(f"prompt: {prompt_2}\nstep: {num_inference_steps}\nstrength: {strength}\nguidance_scale: {guidance_scale}\nseed: {seed}")
+    print(f"prompt: {prompt_2}\nstep: {num_inference_steps_2}\nstrength: {strength_2}\nguidance_scale: {guidance_scale_2}\nseed: {seed}")
+
     image[0].save("sd_2.png")
